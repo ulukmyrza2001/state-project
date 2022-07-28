267b27f220f8cb247537930e98f9ab929a6345f8
Beybars <erkulow26@gmail.com>
2022-07-29 00:34:06 +0600

    create-footer-layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+import Footer from './components/layout/Footer'
 
 const App = () => {
-   return <div>App</div>
+   return (
+      <div>
+         <Div />
+         <Footer />
+      </div>
+   )
 }
+const Div = styled.div`
+   width: 100%;
+   height: 1500px;
+   background-color: black;
+`
 
 export default App
