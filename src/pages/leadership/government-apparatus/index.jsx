dba9e68be272bc86c9901ab841309ce1802ac8b4
erkulow <erkulow26@gmail.com>
2022-10-04 17:09:58 +0600

    fix

--- src/pages/leadership/government-apparatus/index.jsx
+++ src/pages/leadership/government-apparatus/index.jsx
@@ -12,77 +12,73 @@ const GovernmentApparatus = () => {
       dispatch(clientGetData('leadership'))
    }, [])
 
-   return (
+   console.log(governmentApparatus)
+
+   return governmentApparatus.map((item) => (
       <Flex
+         key={item.id}
          width="100%"
          direction="column"
+         gap="10px"
          mobileDirection="column"
-         mobileAlign="center"
-         mobileGap="20px"
+         mobileGap="10px 0"
+         margin="10px 0"
       >
-         {governmentApparatus.map((item) => (
-            <>
-               <h2>{item.positions}</h2>
+         <h3>{item.positions}</h3>
+         <Flex
+            gap="20px"
+            mobileDirection="column"
+            mobileAlign="center"
+            mobileGap="10px 0"
+         >
+            <WrapperImage>
+               <img src={item.fileInformation.photo} alt="" />
+            </WrapperImage>
+            <Flex
+               direction="column"
+               gap="10px"
+               justify="space-between"
+               mobileDirection="column"
+               mobileGap="10px 0"
+            >
                <Flex
-                  key={item.id}
-                  mobileJustify="center"
-                  mobileWrap="wrap"
-                  mobileGap="20px"
-                  mobileDirection="column-reverse"
+                  direction="column"
+                  gap="10px"
+                  mobileDirection="column"
                   mobileAlign="center"
-                  gap="20px"
                >
-                  <Flex
-                     mobileDirection="column"
-                     direction="column"
-                     justify="space-between"
-                     mobileGap="20px"
-                     gap="10px 0"
-                  >
-                     <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Quidem accusamus, accusantium odit iusto
-                        voluptatem ab dolor iste placeat similique assumenda
-                        illum corporis porro eveniet. Explicabo distinctio sint
-                        amet corporis magni. Lorem ipsum dolor sit amet
-                        consectetur, adipisicing elit. Quidem accusamus,
-                        accusantium odit iusto voluptatem ab dolor iste placeat
-                        similique assumenda illum corporis porro eveniet.
-                        Explicabo distinctio sint amet corporis magni. Lorem
-                        ipsum dolor sit amet consectetur, adipisicing elit.
-                        Quidem accusamus, accusantium odit iusto voluptatem ab
-                        dolor iste placeat similique assumenda illum corporis
-                        porro eveniet. Explicabo distinctio sint amet corporis
-                        magni. Lorem ipsum dolor sit amet consectetur,
-                        adipisicing elit. Quidem accusamus, accusantium odit
-                        iusto voluptatem ab dolor iste placeat similique
-                        assumenda illum corporis porro eveniet. Explicabo
-                        distinctio sint amet corporis magni.
-                     </p>
-                     <div>
-                        <p>
-                           <b>Байланыш телефону:</b> {item.phoneNumber} <br />
-                        </p>
-                        <p>
-                           <b>Электрондук почтасы:</b> {item.email}
-                        </p>
-                     </div>
-                  </Flex>
-                  <WrapperImage>
-                     <img src={item.fileInformation.photo} alt="" />
-                     <h4>
-                        {item.firstName} {item.lastName}
-                     </h4>
-                  </WrapperImage>
+                  <h4>
+                     {item.firstName} {item.lastName} {item.patronymic}
+                  </h4>
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Vel animi ea, ipsam natus perferendis fugiat minus. Amet
+                     blanditiis deserunt, temporibus corrupti cum ipsum id, iure
+                     animi velit corporis qui voluptatibus.Lorem ipsum dolor sit
+                     amet consectetur, adipisicing elit. Vel animi ea, ipsam
+                     natus perferendis fugiat minus. Amet blanditiis deserunt,
+                     temporibus corrupti cum ipsum id, iure animi velit corporis
+                     qui voluptatibus.Vel animi ea, ipsam natus perferendis
+                     fugiat minus. Amet blanditiis deserunt, temporibus corrupti
+                     cum ipsum id, iure animi velit corporis qui voluptatibus.
+                  </p>
                </Flex>
-            </>
-         ))}
+               <Flex direction="column" mobileDirection="column">
+                  <p>
+                     <b>Байланыш телефону:</b> {item.phoneNumber} <br />
+                  </p>
+                  <p>
+                     <b>Электрондук почтасы:</b> {item.email}
+                  </p>
+               </Flex>
+            </Flex>
+         </Flex>
       </Flex>
-   )
+   ))
 }
 const WrapperImage = styled.div`
    img {
-      width: 250px;
+      width: 200px;
    }
 `
 export default GovernmentApparatus
